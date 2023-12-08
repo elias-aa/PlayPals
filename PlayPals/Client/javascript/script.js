621892907cf42e6f22ab0108da9a85935eb4087f
Elias A <alissand@sheridancollege.ca>
2023-12-08 15:05:57 -0500

    Backend works with frontend succesfull integration

--- PlayPals/Client/javascript/script.js
+++ PlayPals/Client/javascript/script.js
@@ -8,38 +8,39 @@ fetch("http://localhost:5054/api/Users",{
 .then(data => console.log(data))
 .catch(error => console.log(error));
 
-document.getElementById('createAccountForm').addEventListener('submit', function(event) {
+document.getElementById('createAccountForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
+    try {
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirmPassword').value;
 
-    if (password !== confirmPassword) {
-        alert('Passwords do not match.');
-        return;
-    }
-
-    console.log('Submitting:', email, password); // For debugging
-
-    fetch('http://localhost:5054/api/users/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: email, password: password })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+        if (password !== confirmPassword) {
+            alert('Passwords do not match.');
+            return;
         }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Response received:', data); // For debugging
-        window.location.href = 'Home.html'; // Redirect to sign-in page
-    })
-    .catch((error) => {
+
+        console.log('Submitting:', email, password);
+
+        const response = await fetch('http://localhost:5054/api/Users/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email, password: password })
+        });
+
+        console.log('Response:', response);
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`HTTP error! Status: ${response.status}, Text: ${errorText}`);
+        }
+
+        const data = await response.json();
+        console.log('Success:', data);
+    } catch (error) {
         console.error('Error:', error);
-    });
+    }
 });
--- PlayPals/Models/User.cs
+++ PlayPals/Models/User.cs
@@ -16,37 +16,27 @@ namespace PlayPals.Models
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
 
-        //Create Password Hash and Salt
         public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            //Create Hashing Algorithm
             using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
-                //Set Salt
                 passwordSalt = hmac.Key;
-                //Set Hash
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
 
-        //Verify Password Hash
         public bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
         {
-            //Create Hashing Algorithm
             using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
             {
-                //Compute Hash
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                //Compare Hashes
                 for (int i = 0; i < computedHash.Length; i++)
                 {
-                    //If Hashes Don't Match
                     if (computedHash[i] != storedHash[i])
                     {
                         return false;
                     }
                 }
-                //If Hashes Match
                 return true;
             }
         }
