f60cc6d7af8de4cdf14c3fdee78904efbefa17f2
Fernanda Battig <battigro@sheridancollege.ca>
2023-12-09 10:05:11 -0500

    added editProfile.html - having some issues with redirecting from profile to edit profile. Starting logic soon after this error is fixed

--- PlayPals/Program.cs
+++ PlayPals/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>{
         options.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
@@ -18,12 +18,12 @@ builder.Services.AddDbContext<ApplicationDbContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
